df4e33dccb20411f310d83f428796d7736d2de98
tjrink <tjrink@gmail.com>
2026-03-06 23:07:16 +1300

    Created a country class that is populated by Jackson from the JSON object returned by API call

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # se-internship-assessment-rink
 This repo contains my assessment for the SE internship.
+
+
+# Assumptions
+Test Automation Task
+Scenario 1
+- For this scenario, the user story states the the map builder would like to ensure that their maps are correct.
+- I am assuming that the map builder's main concern is ensuring that their existing maps match current data.
+- To match this assumption, I have written the test to match against the number of countries in the dataset as I began the project (250).
--- src/main/java/com/rink/app/App.java
+++ src/main/java/com/rink/app/App.java
@@ -1,20 +1,43 @@
 package com.rink.app;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rink.client.ApiClient;
+import com.rink.model.Country;
 
 public class App {
 
 	public static void main(String[] args) {
 		ApiClient client = new ApiClient();
 		
+		
+		//Sets the basic fields to be requested when creating country objects
 		ArrayList<String> fields = new ArrayList<>();
 		fields.add("name");
 		fields.add("cca3");
 		fields.add("idd");
-		
-		System.out.println(client.getAllCountries(fields));
+		fields.add("capital");
+		fields.add("languages");
+		fields.add("timezones");
+		fields.add("capitalInfo");
+
+		String json_response = client.getAllCountries(fields);
+
+		try {
+			ObjectMapper objectMapper = new ObjectMapper(); // Creates the object mapper to read the object array
+			List<Country> countries = objectMapper.readValue(json_response, new TypeReference<List<Country>>() {
+			}); //Turns each element in the JSON response into a country object and adds that object to a List
+			
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}		
 	}
 
 }
